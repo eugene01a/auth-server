7c0e6532c73120ad915ab584c5a34d4964e57f2b
Eugene Ichinose <eichinose@telesign.com>
2021-08-25 15:00:23 -0700

    Changes from dev machine

--- scripts/setup_app_server.sh
+++ scripts/setup_app_server.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+docker run -d --name token_auth_server /
+-e POSTGRES_DB=flask_jwt_auth_dev /
+-e POSTGRES_PASSWORD=ONLIN3-ex4m /
+-p 5432:5432 postgres
+
 echo "Initializing database"
 python /Users/eichinose/PycharmProjects/MyHomeServer/auth-server/manage.py init_db
 
@@ -6,9 +11,3 @@ echo "Starting auth server"
 export FLASK_APP=/Users/eichinose/PycharmProjects/MyHomeServer/auth-server/manage.py
 export FLASK_ENV=development
 export FLASK_DEBUG=1
-
-python /Users/eichinose/PycharmProjects/MyHomeServer/auth-server/manage.py init_db
-
-docker run --name auth-dev-db \
-    -p 5432:5432 \
-    -d postgres

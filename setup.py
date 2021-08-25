e21ea59dc28bd240648ba62c080065cef03bf9f1
Eugene Ichinose <eichinose@telesign.com>
2021-08-25 15:01:04 -0700

    Mocing changes from dev machine

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AuthServer',
+    version='',
+    packages=['auth'],
+    url='',
+    license='',
+    author='eichinose',
+    author_email='',
+    description=''
+)
